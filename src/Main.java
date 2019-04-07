a65a2fc9550491684d00ba02ea8f45998397cb20
Elia <eliasulimanov@gmail.com>
2019-04-07 22:07:03 +0300

    Screen size relative

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
-
-import java.awt.Robot;
+import java.awt.*;
 import java.awt.event.InputEvent;
 
 public class Main extends Thread {
+    private static GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+    private static int width = gd.getDisplayMode().getWidth();
+    private static int height = gd.getDisplayMode().getHeight();
     public static void main(String[] args) {
         System.out.println("Mouse control app");
         System.out.println("Elia Sulimanov - 2019");
@@ -16,11 +18,10 @@ public class Main extends Thread {
      * @param x screen x
      * @param y screen y
      */
-    public static void moveMouse(int x, int y) {
+    static void moveMouse(int x, int y) {
         try {
-            // TODO make it relative to screen size
             Robot robot = new Robot();
-            robot.mouseMove(x, y);
+            robot.mouseMove(x * width, y * height);
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -30,7 +31,7 @@ public class Main extends Thread {
     /**
      * Left click
      */
-    public static void leftClick(boolean isDoubleClick) {
+    static void leftClick(boolean isDoubleClick) {
         try {
 
             Robot robot = new Robot();
@@ -49,7 +50,7 @@ public class Main extends Thread {
     /**
      * Right click
      */
-    public static void rightClick() {
+    static void rightClick() {
         try {
 
             Robot robot = new Robot();
